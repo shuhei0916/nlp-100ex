66799a247fe6d7971d62e583686054474ebd4a12
shuhei0916 <shuheiito.stc@gmail.com>
2024-04-05 00:19:17 +0900

    modified 14ex.py

--- 02Unix_Commands/14First_N_lines.py
+++ 02Unix_Commands/14First_N_lines.py
@@ -1,11 +1,11 @@
 N = 10
 
-filename = 'popular-names.txt'
+filename = './data/popular-names.txt'
 
 with open(filename) as f, \
     open('col1.txt', 'w') as f1, open('col2.txt', 'w') as f2:
     for i, line in enumerate(f):
         if i < N:
-            print(line)
+            print(line, end="")
         else:
             break
