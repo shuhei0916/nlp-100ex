c936b57254780494ac592e38a9e03128f0abcee5
shuhei0916 <shuheiito.stc@gmail.com>
2024-05-03 05:34:22 +0900

    言語処理100本ノック第2章完了。16番のファイル分割の課題だけ、ピンと来なかったので飛ばした

--- 02Unix_Commands/19Frequency.py
+++ 02Unix_Commands/19Frequency.py
@@ -1,5 +1,27 @@
+import collections
+
 def main():
-    print(123)
+    filename = './data/popular-names.txt'
+    name_list = []
+    with open(filename, 'r') as f:
+        for line in f:
+            name, sex, num, year = line.rstrip('\n').split(' ')
+            name_list.append(name)
+        
+    name_counts = collections.Counter(name_list)
+    # print(c)
+    for name, count in name_counts.most_common():
+        print(f'{name}: {count}')
+        
 
 if __name__ =='__main__':
     main()
+    
+'''
+UNIXコマンドは以下のようになる（ムズ。。。）
+
+cut -d " " -f 1 ./data/popular-names.txt | sort | uniq -c | sort -n  -r | head -20
+cut -d " " -f 1 : ファイルをスペースで区切り（delimiter -d）、1番目のフィールド（ここでは名前）のみを切り出す。
+sort | uniq: この二つのコマンドを組み合わせることで、重複を除いている（uniqは隣接する同じ要素を除くので、前処理としてsortで並べている）。sortに-cオプションをつけることで、カウントしている。
+sort -n -r: 上記で得られた出力をさらにソートしている。-nで数字としてソートし、-rで逆順（reverse）でソートしている。
+'''
--- 02Unix_Commands/set_test}.py
+++
@@ -1,5 +0,0 @@
-s = {10, 20, 30, 50, 50}
-
-print('hehe')
-print(type(s))
-print(s)
