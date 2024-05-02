dd7345cad5b736a84b8e2f29d29cb4df2f1092a3
shuhei0916 <shuheiito.stc@gmail.com>
2024-05-03 01:39:55 +0900

    modified 02-15Lans_N_lines.py

--- 02Unix_Commands/15Last_N_lines.py
+++ 02Unix_Commands/15Last_N_lines.py
@@ -3,16 +3,26 @@ import sys
 
 N = 10
 
-filename = 'popular-names.txt'
+filename = './data/popular-names.txt'
 
-def tail(N):
-    buf = deque(sys.stdin, N)
-    print(''.join(buf))
+# def tail(N):
+#     buf = deque(sys.stdin, N)
+#     print(''.join(buf))
 
-with open(filename) as f, \
-    open('col1.txt', 'w') as f1, open('col2.txt', 'w') as f2:
-    for i, line in enumerate(f):
-        if i < N:
-            print(line)
-        else:
-            break
+# readlines()で全行をメモリに読み込んでから処理するやり方。メモリ効率は悪い
+# with open(filename) as f:
+#     l = f.readlines()
+#     l = l[-N:]
+    
+#     for i, line in enumerate(l):
+#         if i < N:
+#             print(line, end="")
+#         else:
+#             break
+        
+        
+d = deque(['l', 'm', 'n'], 2)
+print(d)
+        
+# with open(filename) as f:
+#     d = deque(f, N)
